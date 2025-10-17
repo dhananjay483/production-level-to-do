4ea892f1513d5c4125c6f7eb02d4d469e6b4acaf
DhananjayMallik <dhananjaymallik918@gmail.com>
2025-10-17 10:23:57 +0530

    feat: I want add user model

---
+++ src/Model/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+ 
+// Here we define our user model
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    email : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    password : {
+        type : String,
+        required : [true,'Provide a password'],
+        minlength : 8
+    },
+    role : {
+        type : String,
+        required : true,
+        enum : ['user','admin']
+    },
+    otp : {
+        type : String
+    },
+    isVerified : {
+        type : Boolean,
+        default : false
+    },
+    timestamps : true
+});
+const User = mongoose.model('User',userSchema);
+export default User;
